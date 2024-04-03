9bbcfd5e0fe33d92f6cc2e150acc7510c5aaa9db
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-03 10:25:52 -0500

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
-
 def create_app():
-	app = Flask(__name__)
-	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://postgres:hank@localhost:5432/postgres')
-	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-	db.init_app(app)
-	return app
+    app = Flask(__name__)
+    # Specify the PostgreSQL dialect explicitly
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:hank@localhost:5432/postgres')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    return app
 
-
-app = create_app()
-
-# app will not run, could not implement API in time for deadline
 if __name__ == '__main__':
-	app.run()
+    app = create_app()
+    app.run()
