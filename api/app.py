fc15c9f29bed7ca3e2f1c7f905021b1f7a827662
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-03 18:33:35 -0500

    fix

--- api/app.py
+++ api/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'new_password'
-app.config['MYSQL_DB'] = 'bob_ross_database'
+app.config['MYSQL_DB'] = 'database'
 # Update the MySQL plugin to auth_socket
 app.config['MYSQL_AUTH_PLUGIN'] = 'auth_socket'
 
